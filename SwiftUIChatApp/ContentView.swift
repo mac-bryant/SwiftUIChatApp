46e554b388028849aa14b3c0a5da74522bfee572
Mackenzie Bryant <mackenziebryantk@gmail.com>
2020-04-20 10:41:14 -0500

    Initial Commit

---
+++ SwiftUIChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIChatApp
+//
+//  Created by Mackenzie Bryant on 4/20/20.
+//  Copyright © 2020 Mackenzie Bryant. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
